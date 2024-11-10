4386c842213956679da1c6a1224e69da6a8dfbf0
mvpinzon <mv.pinzon@uniandes.edu.co>
2024-11-10 11:54:33 -0500

    ignore_datasets_gitignore

--- model.py
+++ model.py
@@ -1,32 +1,33 @@
 from tensorflow.keras.models import model_from_json
-from tensorflow.python.keras.backend import set_session
+import tensorflow as tf
 import numpy as np
 
-import tensorflow as tf
+# Configuración para limitar el uso de memoria de la GPU
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Configurar TensorFlow para limitar la memoria GPU a un porcentaje
+        for gpu in gpus:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpu,
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])  # Ajusta el límite de memoria en MB
+    except RuntimeError as e:
+        print(e)
 
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.15
-session = tf.compat.v1.Session(config=config)
-set_session(session)
-
-
-class ERModel(object):
-
-    EMOTIONS_LIST = ["Angry", "Disgust",
-                     "Fear", "Happy",
-                     "Neutral", "Sad",
-                     "Surprise"]
+class ERModel:
+    EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
 
     def __init__(self, model_json_file, model_weights_file):
-
+        # Cargar la estructura del modelo desde un archivo JSON
         with open(model_json_file, "r") as json_file:
             loaded_model_json = json_file.read()
             self.loaded_model = model_from_json(loaded_model_json)
 
+        # Cargar los pesos del modelo
         self.loaded_model.load_weights(model_weights_file)
 
     def predict_emotion(self, img):
-        global session
-        set_session(session)
+        # Realizar la predicción
         self.preds = self.loaded_model.predict(img)
+        # Retornar la emoción con mayor probabilidad
         return ERModel.EMOTIONS_LIST[np.argmax(self.preds)]
